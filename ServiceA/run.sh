42a10dd869fe03901c977299d517de8f8bfc0dbc
Chalermrid Patipanang <chalermrid.p@lmwn.com>
2022-04-27 09:34:52 +0700

    add run script

---
+++ ServiceA/run.sh
@@ -0,0 +1,2 @@
+mvn clean install
+java -jar target/OpenTelemetryJaeger.ServiceA-0.0.1-SNAPSHOT.jar
---
+++ ServiceB/run.sh
@@ -0,0 +1,2 @@
+mvn clean install
+java -jar target/OpenTelemetryJaeger.ServiceB-0.0.1-SNAPSHOT.jar
