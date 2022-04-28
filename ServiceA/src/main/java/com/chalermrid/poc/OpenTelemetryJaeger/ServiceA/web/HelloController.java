4e5fc48c5c686b6eea429471c7e26f478bd97ded
Chalermrid Patipanang <chalermrid.p@lmwn.com>
2022-04-28 10:51:56 +0700

    move external hello

--- ServiceA/src/main/java/com/chalermrid/poc/OpenTelemetryJaeger/ServiceA/web/HelloController.java
+++ ServiceA/src/main/java/com/chalermrid/poc/OpenTelemetryJaeger/ServiceA/web/HelloController.java
@@ -1,5 +1,6 @@
 package com.chalermrid.poc.OpenTelemetryJaeger.ServiceA.web;
 
+import com.chalermrid.poc.OpenTelemetryJaeger.ServiceA.service.ExternalService;
 import com.chalermrid.poc.OpenTelemetryJaeger.ServiceA.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,15 +9,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URISyntaxException;
+
 @RestController
 @RequestMapping(path = "/v1/hello")
 public class HelloController {
     @Autowired
     private HelloService helloService;
+    @Autowired
+    private ExternalService externalService;
 
     @GetMapping
     public ResponseEntity<String> getHello() {
         String response = helloService.getHello();
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/external")
+    public ResponseEntity<String> getHelloFromExternal() throws URISyntaxException {
+        String response = externalService.getHello();
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
